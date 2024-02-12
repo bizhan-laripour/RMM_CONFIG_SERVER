56a9c4ee278e47fdad10144a8a1ff8c771874a17
bizhan <bizhan.laripour.herat@gmail.com>
2024-02-12 17:11:30 +0330

    first

--- src/main/java/org/config/Main.java
+++ src/main/java/org/config/Main.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 @EnableConfigServer
 public class Main {
+
     public static void main(String[] args) {
         SpringApplication.run(Main.class , args);
     }
